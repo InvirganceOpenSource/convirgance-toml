c71b6c952cf301e13d30651a72af263b4d51a18f
Timur Kasimov <kasimovmoor@gmail.com>
2025-04-08 20:33:39 -0500

    first commit

---
+++ src/main/java/com/invirgance/convirgance/toml/ConvirganceToml.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.invirgance.convirgance.toml;
+
+/**
+ *
+ * @author timur
+ */
+public class ConvirganceToml {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
